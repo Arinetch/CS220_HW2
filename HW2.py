7d22c04655c7c7c62d39928923406b4e674d1dc4
Arinetch <Arine_jokaklian@edu.aua.am>
2019-09-30 22:26:06 +0400

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -1,6 +1,5 @@
 import shutil
 import os
-import time
 
 def task(source, destination):
     files = os.listdir(source)
@@ -11,10 +10,7 @@ my_source = '/Users/mac/Desktop/Desktop/AUA/Fall 2019/Parallel HPC/Python HW/unt
 my_destination = '/Users/mac/Desktop/Desktop/AUA/Fall 2019/Parallel HPC/Python HW/untitled folder 2/'
 
 def main():
-    start = time.time()
     task(my_source, my_destination)
-    end = time.time()
-    print(end - start)
 
 if __name__ == '__main__':
     main()
